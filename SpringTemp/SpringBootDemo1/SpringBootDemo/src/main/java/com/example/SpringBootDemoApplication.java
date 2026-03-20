c20560918fb5d76188f71a4ce18f5e6682c81862
sarthak76342 <sarthak22102003@gmail.com>
2026-03-20 12:00:17 +0530

    Day 39 - SpringBoot First Project

---
+++ SpringTemp/SpringBootDemo1/SpringBootDemo/src/main/java/com/example/SpringBootDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+	
+	
+}
